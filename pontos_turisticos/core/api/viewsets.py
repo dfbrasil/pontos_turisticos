4a942f8eee397e2c2aca9b3b2db7b2a86d72bbe5
Daniel Brasil <danieldfb@gmail.com>
2022-07-22 11:02:53 -0300

    mecanismos de busca

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,11 +1,14 @@
 
 from argparse import Action
 from crypt import methods
+from urllib import request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+
 
 
 class PontoTuristicoViewSet(ModelViewSet):
@@ -13,9 +16,27 @@ class PontoTuristicoViewSet(ModelViewSet):
     #queryset = PontoTuristico.objects.all() #também poderia passar o argumento aqui
     #queryset = PontoTuristico.objects.filter(aprovado = True)
     serializer_class = PontoTuristicoSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ['nome','descricao','endereco__linha1']
+    #lookup_field = 'nome' está comentado pois pode dar erro se tiver mais de um retorno
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado = True)
+        id=self.request.query_params.get('id',None)
+        nome=self.request.query_params.get('nome',None)
+        descricao=self.request.query_params.get('descricao',None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(id=id)
+        
+        if nome:
+            queryset = queryset.filter(nome__iexact=nome)
+
+        if descricao:
+            queryset = queryset.filter(descricao__iexact=descricao)
+
+        return queryset
+        # return PontoTuristico.objects.filter(aprovado = True)
 
     def list(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
